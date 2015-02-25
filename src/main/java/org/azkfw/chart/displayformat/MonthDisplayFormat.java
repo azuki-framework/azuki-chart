61d1dcea5a832095b4c8a9507597c90122d15119
kawakicchi <azuki.framework@gmail.com>
2015-02-25 14:36:36 +0900

    [fix] 月表示フォーマットの不具合修正
    - 月の表示を0始まりから1始まりへ修正

--- src/main/java/org/azkfw/chart/displayformat/MonthDisplayFormat.java
+++ src/main/java/org/azkfw/chart/displayformat/MonthDisplayFormat.java
@@ -33,29 +33,29 @@ public class MonthDisplayFormat implements DisplayFormat {
 	public String toString(final double aValue) {
 		int value = (int) aValue;
 		switch (value) {
-		case 0:
-			return "Jan";
 		case 1:
-			return "Feb";
+			return "Jan";
 		case 2:
-			return "Mar";
+			return "Feb";
 		case 3:
-			return "Apr";
+			return "Mar";
 		case 4:
-			return "May";
+			return "Apr";
 		case 5:
-			return "Jun";
+			return "May";
 		case 6:
-			return "Jul";
+			return "Jun";
 		case 7:
-			return "Aug";
+			return "Jul";
 		case 8:
-			return "Sep";
+			return "Aug";
 		case 9:
-			return "Oct";
+			return "Sep";
 		case 10:
-			return "Nov";
+			return "Oct";
 		case 11:
+			return "Nov";
+		case 12:
 			return "Dec";
 		}
 		return null;
